69cdc76ee64e8181ed0563aeee9016d194343a0a
anraku <anraku90@gmail.com>
2018-12-31 15:46:57 +0900

    add infix expression

--- ast/ast.go
+++ ast/ast.go
@@ -86,6 +86,27 @@ func (pe *PrefixExpression) String() string {
 	return out.String()
 }
 
+type InfixExpression struct {
+	Token    token.Token // 演算子トークン、例えば「+」
+	Left     Expression
+	Operator string
+	Right    Expression
+}
+
+func (ie *InfixExpression) expressionNode()      {}
+func (ie *InfixExpression) TokenLiteral() string { return ie.Token.Literal }
+func (ie *InfixExpression) String() string {
+	var out bytes.Buffer
+
+	out.WriteString("(")
+	out.WriteString(ie.Left.String())
+	out.WriteString(ie.Operator)
+	out.WriteString(ie.Right.String())
+	out.WriteString(")")
+
+	return out.String()
+}
+
 // TokenLiteral is root not in AST
 func (p *Program) TokenLiteral() string {
 	if len(p.Statements) > 0 {
