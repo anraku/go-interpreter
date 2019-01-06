876e20e7a5b20fdbc2185372c354f3dbf3dc83f0
anraku <anraku90@gmail.com>
2019-01-06 19:36:45 +0900

    add test infixExpression

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -272,3 +272,34 @@ func TestParsingInfixExpressions(t *testing.T) {
 		}
 	}
 }
+
+func TestOperatorPrecedenceParsing(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected string
+	}{
+		{"-a * b", "((-a)*b)"},
+		{"!-a", "(!(-a))"},
+		{"a + b + c", "((a+b)+c)"},
+		{"a + b - c", "((a+b)-c)"},
+		{"a * b * c", "((a*b)*c)"},
+		{"a * b / c", "((a*b)/c)"},
+		{"a + b / c", "(a+(b/c))"},
+		{"a + b * c + d / e - f", "(((a+(b*c))+(d/e))-f)"},
+		{"3 + 4; -5 * 5", "(3+4)((-5)*5)"},
+		{"5 > 4 == 3 < 4", "((5>4)==(3<4))"},
+		{"5 > 4 != 3 < 4", "((5>4)!=(3<4))"},
+		{"3 + 4 * 5 == 3 * 1 + 4 * 5", "((3+(4*5))==((3*1)+(4*5)))"},
+	}
+	for i, tt := range tests {
+		l := lexer.New(tt.input)
+		p := New(l)
+		program := p.ParseProgram()
+		checkParseErrors(t, p)
+
+		actual := program.String()
+		if actual != tt.expected {
+			t.Fatalf("tests[%d] - hoge wrong. expected=%q, got=%q", i, tt.expected, actual)
+		}
+	}
+}
