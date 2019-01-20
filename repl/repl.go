01429bd37722865877988b29b4f6894f3d8b7900
anraku <anraku90@gmail.com>
2019-01-20 19:16:30 +0900

    add evaluator and object

--- repl/repl.go
+++ repl/repl.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 
+	"github.com/anraku/monkey/evaluator"
 	"github.com/anraku/monkey/lexer"
 	"github.com/anraku/monkey/parser"
 )
@@ -31,8 +32,12 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		io.WriteString(out, program.String())
-		io.WriteString(out, "\n")
+		evaluator := evaluator.Eval(program)
+
+		if evaluator != nil {
+			io.WriteString(out, evaluator.Inspect())
+			io.WriteString(out, "\n")
+		}
 	}
 }
 
